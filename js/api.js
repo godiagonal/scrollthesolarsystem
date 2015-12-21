9f3048a956b77bad126331ab049cf8984aeb6833
godiagonal <same_johansson@hotmail.com>
2015-12-22 00:00:29 +0100

    Added spinner to make UX more pleasant.

--- js/api.js
+++ js/api.js
@@ -4,15 +4,22 @@ var api = {
 	url: 'data.php',
 
 	getPlanetData: function () {
+		
+		// Start loading spinner
+		$('#spinner').show();
 
 		$.getJSON(api.url, function (data) {
 
+			// Stop spinner
+			$('#spinner').hide();
+			
 			api.planetData = data;
 			guiOverview.updateData();
 			guiExploration.updateData();
 
 		}).fail(function () {
 
+			$('#spinner').hide();
 			alert('An error occured when loading planetary data.');
 
 		});
